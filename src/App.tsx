aa543811ec7080ead17ba3f6a7fcd6a17bb95ba1
Dylan <cerobreath@gmail.com>
2025-04-21 19:48:54 +0300

    feat: removing old samples for a completely clean project architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Toaster } from "@/components/ui/toaster";
+
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -54,7 +54,6 @@ const AnimatedRoutes = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <BrowserRouter>
         <ScrollToTop />
